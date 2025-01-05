85ae4ff8d20bd3be34073094418711e5ec212c89
moawezz <moawez.de@gmail.com>
2025-01-05 19:41:22 +0330

    add context for handling states

--- core/context.py
+++ core/context.py
@@ -1,22 +1,37 @@
-import logging
+from core.driver_factory import DriverFactory
+from states.login_state import LoginState
 
-def get_logger(name):
-    # Create a logger
-    logger = logging.getLogger(name)
+class GolestanGradeCheckerBot:
+    def __init__(self, config, driver=None, browser_name="chrome"):
+        self.config = config
+        self.browser_name = browser_name
+        self.driver = driver if driver else DriverFactory.get_driver(self.browser_name)
+        self.state = LoginState(self)
+
+    def get_context_driver(self):
+        """Initialize driver only if it doesn't exist or is unusable"""
+        if not self.driver:
+            print("Creating new WebDriver instance")
+            self.driver = DriverFactory.get_driver(self.browser_name)
+        else:
+            try:
+                # Check if driver is still alive by accessing a simple attribute
+                print("Reusing existing WebDriver instance")
+                self.driver.title  # This will raise an error if the driver is closed
+            except Exception as e:
+                print(f"Driver seems to be closed or unusable. Error: {e}")
+                print("Creating new WebDriver instance")
+                self.driver = DriverFactory.get_driver(self.browser_name)
+        return self.driver
     
-    # Set the logging level (change to DEBUG, INFO, WARNING, ERROR, etc. as needed)
-    logger.setLevel(logging.DEBUG)
-    
-    # Create console handler and set level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    
-    # Create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    
-    # Add the handler to the logger if it doesn't already have it
-    if not logger.hasHandlers():
-        logger.addHandler(ch)
-    
-    return logger
+    def change_state(self, new_state):
+        self.state = new_state
+
+    def run(self):
+        self.get_context_driver()
+
+        while True:
+            try:
+                self.state.handle()
+            except Exception as e:
+                print(f"Error during bot run: {e}")
